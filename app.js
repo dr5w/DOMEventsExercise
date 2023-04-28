6b7e2bc925e118e38a7179d2b956ce9cd8b5404b
dr5w <111304827+dr5w@users.noreply.github.com>
2023-04-28 10:58:12 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// 1a. 
+const button1 = document.querySelector(`button`);
+// 1b. 
+button1.onclick = () => {
+    alert("You clicked the first button! Congrats!");
+}
+// 2a.
+const h3 = document.querySelector(`h3`);
+// 2b.
+h3.addEventListener(`mouseover`, () => {
+  alert("You hovered over the h3 element! Congrats!");
+});
+// 3a.
+const form = document.querySelector(`form`);
+// 3b.
+form.addEventListener(`submit`, e =>{
+  e.preventDefault();
+  alert(form.elements.entry.value);
+  form.elements.entry.value = "";
+});
+
+// Bonus
+// 4a.
+const darkMode = document.querySelector(`#dm`);
+// 4b.
+const body = document.querySelector(`body`);
+darkMode.addEventListener(`click`, () => {
+  body.classList.toggle(`dark-mode`);
+});
+// 5a.
+const times = document.querySelector(`#times`);
+// 5b. 
+let num = 1;
+times.addEventListener(`click`, () => {
+  if (num < 3){
+    alert(`You clicked the last button! Congrats!`);
+  } else if (num === 3){
+    alert("OH NO! This button is NOT going to work anymore");
+  }
+  num++;
+});
